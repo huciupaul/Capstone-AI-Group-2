ac6083c673d81d6f38be684fad64359f16c3f506
huciupaul <huciupaul3@gmail.com>
2024-12-11 11:53:04 +0100

    generated_data_12000_steps_20mins

--- input_generator.py
+++ input_generator.py
@@ -5,59 +5,64 @@ import numpy as np
 import h5py
 import tqdm
 
-# np.random.seed(42)
 
-# # instantiate solver
-# ks = KolSol(nk=8, nf=4, re=40.0, ndim=2) # nk = no of symmetric wavenumbers   nf = forcing frequency, re=40 fully turbulent flow
-# dt = 0.01
-# steps = int(20.0 / dt)
-# store_interval = 10  # Store every 10 steps
-# n_stored = steps // store_interval
-# # the timestep to store the resuls is 0.1
+np.random.seed(42)
 
-# # define initial conditions
-# u_hat = ks.random_field(magnitude=10.0, sigma=2.0, k_offset=[0, 3])
 
-# # simulate :: run over transients
-# for _ in np.arange(0.0, 180.0, dt):
-#   u_hat += dt * ks.dynamics(u_hat)
+# instantiate solver
+ks = KolSol(nk=8, nf=4, re=40.0, ndim=2) # nk = no of symmetric wavenumbers   nf = forcing frequency, re=40 fully turbulent flow
+dt = 0.01
+steps = int(12000.0 / dt)
+store_interval = 10  # Store every 10 steps
+n_stored = steps // store_interval
+# the timestep to store the resuls is 0.1
 
-# # simulate :: generate results
 
-# K = np.zeros(n_stored)
-# D = np.zeros(n_stored)
-# time = np.zeros(n_stored)
-# u_field_all = np.zeros((n_stored, 48, 48, 2))  # Assuming u_field is (48, 48, 2)
+# define initial conditions
+u_hat = ks.random_field(magnitude=10.0, sigma=2.0, k_offset=[0, 3])
 
-# # Simulate :: generate results
-# store_idx = 0
-# for step, t in enumerate(np.arange(0.0, 3000.0, dt)):
-#     u_hat += dt * ks.dynamics(u_hat)
-#     if step % store_interval == 0:  # Store every 10 steps
-#         time[store_idx] = t
-#         D[store_idx] = ks.dissip(u_hat)
-#         K[store_idx] = 0.5 * np.sum(np.abs(u_hat)**2)
-#         u_field_all[store_idx] = ks.fourier_to_phys(u_hat, nref=48)  # Compute physical field
-#         store_idx += 1
 
-# def write_h5(path: str, velocity: np.ndarray, kinetic_energy: np.ndarray, dissipation_rates: np.ndarray) -> None:
-#     with h5py.File(path, 'w') as hf:
-#         # Save velocity as a dataset
-#         hf.create_dataset("velocity_field", data=velocity, compression="gzip")
+# simulate :: run over transients to stabilize
+for _ in np.arange(0.0, 200.0, dt):
+  u_hat += dt * ks.dynamics(u_hat)
 
-#         # Save kinetic energy list
-#         hf.create_dataset("kinetic_energy", data=kinetic_energy, compression="gzip")
 
-#         # Save dissipation rates list
-#         hf.create_dataset("dissipation_rates", data=dissipation_rates, compression="gzip")
-#     print(f"Results saved to {path}")
+# data initialization
+K = np.zeros(n_stored)
+D = np.zeros(n_stored)
+time = np.zeros(n_stored)
+u_field_all = np.zeros((n_stored, 48, 48, 2))  # Assuming u_field is (48, 48, 2)
+
+
+# Simulate :: generate results
+store_idx = 0
+for step, t in enumerate(np.arange(0.0, 12000.0, dt)):
+    u_hat += dt * ks.dynamics(u_hat)
+    if step % store_interval == 0:  # Store every 10 steps
+        time[store_idx] = t
+        D[store_idx] = ks.dissip(u_hat)
+        K[store_idx] = 0.5 * np.sum(np.abs(u_hat)**2)
+        u_field_all[store_idx] = ks.fourier_to_phys(u_hat, nref=48)  # Compute physical field
+        store_idx += 1
+
+
+def write_h5(path: str, velocity: np.ndarray, kinetic_energy: np.ndarray, dissipation_rates: np.ndarray) -> None:
+    with h5py.File(path, 'w') as hf:
+        # Save velocity as a dataset
+        hf.create_dataset("velocity_field", data=velocity, compression="gzip")
+
+        # Save kinetic energy list
+        hf.create_dataset("kinetic_energy", data=kinetic_energy, compression="gzip")
+
+        # Save dissipation rates list
+        hf.create_dataset("dissipation_rates", data=dissipation_rates, compression="gzip")
+    print(f"Results saved to {path}")
   
-# write_h5("D:\ml_midterm\Endterm\Capstone-AI-Group-2\Generated_data.h5", u_field_all, K, D)
+write_h5("D:\ml_midterm\Endterm\Capstone-AI-Group-2\Generated_data.h5", u_field_all, K, D)
 
 
-
-# # Verify the shapes of the stored arrays
-# print(f"D shape: {D.shape}, time shape: {time.shape}, u_field_all shape: {u_field_all.shape}")
+# Verify the shapes of the stored arrays
+print(f"D shape: {D.shape}, time shape: {time.shape}, u_field_all shape: {u_field_all.shape}")
     
 
 with h5py.File("D:\ml_midterm\Endterm\Capstone-AI-Group-2\Generated_data.h5", "r") as h5file:
@@ -66,13 +71,22 @@ with h5py.File("D:\ml_midterm\Endterm\Capstone-AI-Group-2\Generated_data.h5", "r
     kinetic_energy = h5file["kinetic_energy"][:]
 
 
-
-
 # Plot the dissipation rate
-plt.figure(figsize=(12, 6))
-plt.plot(dissipation, label="Dissipation Rate")
+plt.figure(figsize=(15, 6))
+plt.plot(time, dissipation, label="Dissipation Rate")
 plt.xlabel("Time")
 plt.ylabel("Magnitude")
 plt.legend()
 plt.title("Dissipation Rate Over Time (Every 10 Steps)")
+plt.savefig('dissip_plot.png', dpi=500, bbox_inches='tight')  # Save as a PNG file
+plt.show()
+
+# Plot the kinetic energy
+plt.figure(figsize=(15, 6))
+plt.plot(time, kinetic_energy, label="Kinetic energy")
+plt.xlabel("Time")
+plt.ylabel("Magnitude")
+plt.legend()
+plt.title("Kinetic energy Over Time (Every 10 Steps)")
+plt.savefig('KE_plot.png', dpi=500, bbox_inches='tight')  # Save as a PNG file
 plt.show()
