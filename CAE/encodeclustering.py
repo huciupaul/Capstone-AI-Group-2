8385de57481cee66fdcfb250c71481e8fe8ebe8e
edlyn <EdlynAbneshaSanthaKumaran@student.tudelft.nl>
2025-01-17 09:56:09 +0100

    encoding for clustering

--- CAE/encodeclustering.py
+++ CAE/encodeclustering.py
@@ -1,6 +1,6 @@
 """
-This script is independent of the rest of the infrastructure. 
-It encodes data into a latent space and saves the encoded representation. 
+This script is independent of the rest of the infrastructure.
+It encodes data into a latent space and saves the encoded representation.
 Variables in this file do not interact with or affect variables in the rest of the codebase.
 """
 
@@ -15,13 +15,16 @@ from constants import *
 enc_path = './data/48_RE40_' + str(n_lat)  # to save model
 enc_mods = load_encoder(enc_path)
 
-# load data
-data_path = 'data\Generated_data.h5'
-U = load_data(data_path)
 
-# batch data
-batch_size = 50
-n_batch = len(U) // batch_size
+# Path to the dataset
+data_path = r"C:\Users\Rafael Ribeiro\Desktop\Capstone\CAE\generated_data_600000_5.h5"
+
+# Load data
+U = load_data(data_path, data_len=120000, downsample=1, transient=0)[-8000:]
+batch_size = 200
+n_batch = 40
+
+# batch U
 U = batch_data(U, batch_size, n_batch)
 
 # forward pass through encoder in batches, save without batches
@@ -32,7 +35,7 @@ for i in range(n_batch):
 print(U_enc.shape)
 
 # save encoded data
-enc_file = f'./data/48_Encoded_data_Re40_{n_lat}.h5'
+enc_file = f'./data/48_Encoded_data_Re40_{n_lat}_17_1.h5'
 hf = h5py.File(enc_file, 'w')
 hf.create_dataset('U_enc', data=U_enc)
 hf.close()
