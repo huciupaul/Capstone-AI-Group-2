694f26da1dc714329ada94987d75612c5659da75
RafaBRib <160110490+RafaBRib@users.noreply.github.com>
2025-01-24 14:30:11 +0100

    mse plot

--- CAE/test_visuals.py
+++ CAE/test_visuals.py
@@ -1,6 +1,6 @@
 from visualization import plot_training_curve, read_mse_plot, plot_hyperparameter_tuning
 
- vloss_plot, tloss_plot = read_mse_plot("mse_plot_40.h5")
+vloss_plot, tloss_plot = read_mse_plot("mse_plot_10_100.h5")
 plot_training_curve(vloss_plot, tloss_plot, 50, n_lat=10)
 
 # plot_hyperparameter_tuning('hyperparameter_tuning.txt')
